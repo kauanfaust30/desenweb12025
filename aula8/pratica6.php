84a8796468985427871a3b1a4d220abce0359ca6
kauanfaust30 <kauan.faust@unidavi.edu.br>
2025-09-30 13:28:48 -0300

    Add files via upload
    Utilização de html, css em conjunto com array em php

--- aula8/pratica6.php
+++ aula8/pratica6.php
@@ -1,4 +1,61 @@
-<?php
-    $disciplina = array(array()
-    )
-?>
+<html>
+<head>
+    <title>Prática 6 - Aula 8</title>
+    <meta charset="utf-8">
+</head>
+<body>
+<h3>Boletim</h3>
+
+<table >
+    <tr class="header">
+        <th>Disciplina</th>
+        <th>Faltas</th>  
+        <th>Nota</th>
+    </tr>   
+        <?php
+            $disciplina = array(
+                            array("Matemática","5","8.5"),
+                            array("Português","2","9"),
+                            array("Geografia","10","6"),
+                            array("Educação Física","2","8")
+                    );
+
+                    
+            foreach($disciplina as $chave) {
+                echo "<tr>";
+                foreach($chave as $d) {
+                    echo "<td>" . $d . "</td>";
+                }
+                echo "</tr>";
+            }
+        ?>
+</table> 
+<style>
+    table, th, td {
+        padding: 5px;
+        align-items: left; 
+        width: 40%;   
+    }
+    th, td {
+        padding: 5px;
+        text-align: left;       
+    }
+    body{
+        display: flex;
+        flex-direction: column; 
+        align-items: center;    
+    }
+    h3{
+        text-align: center; 
+    }
+    tr{
+        background-color: #c2e1f6ff;    
+    }
+    .header {
+        background-color: #4595dbff;
+        color: white;
+    }
+</style>
+</body>
+
+</html>
