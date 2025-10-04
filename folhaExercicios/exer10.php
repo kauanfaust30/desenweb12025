f09d99af69030ebd991d90bad9c9f9e64b9bedd9
kauanfaust30 <kauan.faust@unidavi.edu.br>
2025-10-03 21:41:15 -0300

    Add files via upload

--- folhaExercicios/exer10.php
+++ folhaExercicios/exer10.php
@@ -1,21 +1,23 @@
 <?php
-    $pastas =array("bsn" =>
-                array("3a Fase" =>
-                    array("desenvWeb","bancoDados 1", "engSoft 1"),
-                "4a Fase" =>
-                     array("Intro Web","bancoDados 2", "engSoft 2"))
-            );
+    $pastas = array(
+        "bsn" => array(
+            "3a Fase" => array("desenvWeb", "bancoDados 1", "engSoft 1"),
+            "4a Fase" => array("Intro Web", "bancoDados 2", "engSoft 2")
+        )
+    );
 
-    function listaPastas($pasta, int $nivel =1) {
-        if(is_array($pasta)){
-            foreach($pasta as $elemento => $valor){
-                echo str_repeat("-",$nivel) . "$elemento <br>";
-
-                listaPastas($valor,$nivel + 1);
+    function listaPastas($pasta, int $nivel = 1) {
+        if (is_array($pasta)) {
+            foreach ($pasta as $chave => $valor) {
+                if (is_array($valor)) {
+                    echo str_repeat("-", $nivel) . $chave . "<br>";
+                    listaPastas($valor, $nivel + 1);
+                } else {
+                    echo str_repeat("-", $nivel) . $valor . "<br>";
+                }
             }
-        } else {
-            echo str_repeat("-",$nivel) . "$pasta <br>";
         }
     }
+
     listaPastas($pastas);
 ?>
